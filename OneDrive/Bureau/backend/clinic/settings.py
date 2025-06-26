66900206031e4ddfe3c1eaec4a25df293416f4cf
ChahrazedBz <chahrazedamina191@gmail.com>
2025-06-26 19:39:37 +0100

    port

--- OneDrive/Bureau/backend/clinic/settings.py
+++ OneDrive/Bureau/backend/clinic/settings.py
@@ -38,15 +38,14 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core",
-    'rest_framework',
-    'corsheaders',
-    'accounts',
-    
+    "rest_framework",
+    "corsheaders",
+    "accounts",
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -62,7 +61,7 @@ ROOT_URLCONF = "clinic.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        'DIRS': [BASE_DIR / 'templates'],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -129,30 +128,30 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'medeciels@gmail.com'  
-EMAIL_HOST_PASSWORD = 'q6zrmp2c5tmysipugt25a5xfhrgavoza' 
-DEFAULT_FROM_EMAIL = 'medeciels@gmail.com'
-SERVER_EMAIL = 'medeciels@gmail.com'
+EMAIL_HOST_USER = "medeciels@gmail.com"
+EMAIL_HOST_PASSWORD = "q6zrmp2c5tmysipugt25a5xfhrgavoza"
+DEFAULT_FROM_EMAIL = "medeciels@gmail.com"
+SERVER_EMAIL = "medeciels@gmail.com"
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  
+    "http://localhost:5173",
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
     ],
 }
 
-AUTH_USER_MODEL = 'accounts.CustomUser'
+AUTH_USER_MODEL = "accounts.CustomUser"
